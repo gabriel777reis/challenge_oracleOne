e297ccf0b69c019b2599349f417a478016c663e0
gr.dacunha <cunhagabriel23@gmail.com>
2022-01-18 18:51:35 -0300

    foi adicionado a função criptografar e descriptografar

--- scripts/main.js
+++ scripts/main.js
@@ -1,15 +1,45 @@
 
-
-
  function botaocriptografia (){
+   //essa var recebe o que foi escrito dentro da label  
+  var capturaInput = document.getElementById("armazenainput").value;
+   criptografa = capturaInput;
 
-    alert("botao criptografia");
+   var armazenaCriptografia = criptografa ;
+   var mapObj ={
+      e:"enter",
+      i:"imes",
+      a:"ai",
+      o:"ober",
+      u:"ufat"
+   };
+  
+   armazenaCriptografia = armazenaCriptografia.replace(/e|i|a|o|u/gi, function(matched){
+      return mapObj[matched]
+   });
 
- }
+   alert(armazenaCriptografia);
+ }  
 
- 
  function botaodescriptografia (){
+   var capturaInput = document.getElementById("armazenainput").value;
+      descriptografa = capturaInput;
 
-    alert("botao descriptografia");
+      var armazenaDescriptografia = descriptografa ;
+   var mapObj ={
+      enter:"e",
+      imes:"i",
+      ai:"a",
+      ober:"o",
+      ufat:"u"
+   };
+  
+   armazenaDescriptografia = armazenaDescriptografia.replace(/enter|imes|ai|ober|ufat/gi, function(matched){
+      return mapObj[matched]
+   });
+
+   alert(armazenaDescriptografia);
+
 
  }
+
+
